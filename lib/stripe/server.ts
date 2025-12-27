8affac80aff2cce977d5d1e30c4f91c2ca99056f
JJAllanLLC <JJAllanLLC@gmail.com>
2025-12-26 21:36:42 -0500

    fix deploy 2

--- lib/stripe/server.ts
+++ lib/stripe/server.ts
@@ -1,7 +1,8 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-11-20.acacia',
+  // Using stable API version for build compatibility (experimental versions not recognized by TypeScript types)
+  apiVersion: '2024-11-20',
   typescript: true,
 });
 
